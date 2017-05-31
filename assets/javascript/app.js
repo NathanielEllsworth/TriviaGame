3b8688368879020d7d9f5242256ab9772b51248f
NathanielEllsworth <nathanielellsworth@gmail.com>
2017-05-31 08:54:37 -0400

    Javascript layout, added html reference links

---
+++ assets/javascript/app.js
@@ -0,0 +1,87 @@
+
+
+//Variables of question answers and number of questions
+// **ask if question.length is needed to avoid hard coding question array length**
+
+// $(document).ready(function(){});
+
+// make array of questions with attributes that resemble JSON pretty print data
+ 
+/**
+
+{
+	
+
+
+},
+{
+	
+
+
+
+
+},
+{
+	
+
+
+
+}, etc..
+
+*/
+
+// figure out timer situation, time is in ms so 1000 is 1 second so 20000 is 20 seconds
+
+// use the same logic from psych project to tackle right / wrong answers
+// if time runs out add it to an 'unanswered' questions list 
+
+
+// add the buttons on click function: $('buttons').on("click", function(){});
+
+// look at your codecadamey code 
+
+
+// ask how to avoid nesting redundent if statements for each question. Don't overthink this.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// be nice to yourself today.
+// I'm proud of you
+// just keep swimming.
